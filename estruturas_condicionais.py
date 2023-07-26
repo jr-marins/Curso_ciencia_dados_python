d588c9e5819d190171628a735b01e422934d9ad3
Marcos <marcosmarinsjr@gmail.com>
2023-07-25 21:14:26 -0300

    atualiazando

--- estruturas_condicionais.py
+++ estruturas_condicionais.py
@@ -1,5 +1,6 @@
 MAIOR_IDADE = 18
 #declaração da constante
+IDADE_ESPECIAL = 12
 
 idade = int(input("Digite sua idade: "))
 
@@ -9,5 +10,9 @@ if idade >= MAIOR_IDADE:
 if idade < MAIOR_IDADE:
     print("Você é um bb, não pode ter habilitação.")
           
-
-# Aqui temos as duas condições, um verdadeira e a falsa ambas retornam uma mensagem, cada qual a seu respeito.
+if idade == IDADE_ESPECIAL:
+    print("você pode fazer as teóricas mais não as práticas")
+    
+else:
+    print("opção inváalida")
+# Aqui temos as tres condições, um verdadeira e a falsa ambas retornam uma mensagem, cada qual a seu respeito.
