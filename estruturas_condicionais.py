99b60ed05e49f7ac30a420c5856eccc5219cb1a8
Marcos <marcosmarinsjr@gmail.com>
2023-07-25 21:00:14 -0300

    estruturas_condicional

---
+++ estruturas_condicionais.py
@@ -0,0 +1,13 @@
+MAIOR_IDADE = 18
+#declaração da constante
+
+idade = int(input("Digite sua idade: "))
+
+if idade >= MAIOR_IDADE:
+    print("Pode tirar habilitação!")
+
+if idade < MAIOR_IDADE:
+    print("Você é um bb, não pode ter habilitação.")
+          
+
+# Aqui temos as duas condições, um verdadeira e a falsa ambas retornam uma mensagem, cada qual a seu respeito.
