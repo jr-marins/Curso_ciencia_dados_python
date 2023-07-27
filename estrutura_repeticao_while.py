1908d06b8283ad8dd5925881e4023665f7149204
Marcos <marcosmarinsjr@gmail.com>
2023-07-26 22:21:27 -0300

    estrutura_repeticao_condicional

---
+++ estrutura_repeticao_while.py
@@ -0,0 +1,9 @@
+opcao = -1
+
+while opcao != 0:
+    opcao = int(input("[1] sacar \n[2] imprimindo extrato\n[0]sair \n:" ))
+
+    if opcao == 1:
+        print("sacando …")
+    elif opcao == 2:
+        print("imprimindo extrato …")
