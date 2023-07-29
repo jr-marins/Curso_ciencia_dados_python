8da0515b238d6b6d229698fd719b7d29f95a85ba
Marcos <marcosmarinsjr@gmail.com>
2023-07-29 15:42:00 -0300

    selecionando_casa

--- string_2.py
+++ string_2.py
@@ -2,9 +2,11 @@ nome = "marcos"
 idade = 24
 profissao ="programador"
 linguagem = "python"
+saldo = 38.556
 #concatenando meu codigo
 
 dados = {'nome': 'marcos','idade': 24, 'profissao': 'programador', 'linguagem': 'pyhon'}
+#meu dicionario
 
 print(" nome? \n%s\n idade? \n%d\n trabalha como ?\n%s\n estuda alguma linguagem? \n%s\n" % (nome, idade, profissao, linguagem))
 
@@ -16,3 +18,7 @@ print(" nome? {nome}\n idade?: {idade}\n profissao: {profissao}\n linguagem: {li
 #usando dados
 
 print(f" nome? {nome}\n idade? {idade}\n trabalha como ? {profissao}\n estuda alguma linguagem? {linguagem}\n".format(nome, idade, profissao, linguagem))
+#usando format
+
+print(f" nome? {nome}\n idade? {idade}\n trabalha como ? {profissao}\n estuda alguma linguagem? {linguagem}\n saldo{saldo:20.1f}")
+#Aqui ele conta as casas da direita para esquerda, eu pedi uma casa depois da letra f. Ele me retornou o 6 pois é a primeira casa da direita para esquerda
