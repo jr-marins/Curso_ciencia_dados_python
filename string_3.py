c0ae71d0521ef6dfb245cad7d73ae000c31a28f6
Marcos <marcosmarinsjr@gmail.com>
2023-07-29 16:19:05 -0300

    strings_multiplas

---
+++ string_3.py
@@ -0,0 +1,16 @@
+nome = "Marcos"
+
+mensagem = f"""
+    Olá, meu nome é {nome},
+Eu estou estudando Python.
+    No meu curso eu aprendi sobre:
+
+##### métodos úteis da classe #####
+   
+   #### Interpolação de variáveis ####
+
+## Fatiamento de string ##
+
+   # String multiplas # """
+
+print(mensagem)
