3bede2e95619751171718c48cb7a0296e62ab2a8
Marcos <marcosmarinsjr@gmail.com>
2023-07-29 13:06:08 -0300

    manipulando_string

---
+++ string_1.py
@@ -0,0 +1,27 @@
+#Manipulando strings
+nome = "marcos"
+
+print(nome.upper())
+#todos os caracters em maiuscula
+print(nome.lower())
+#todoa es minuscula
+print(nome.title())
+#todos em minusculo exceto o primeiro caracter
+
+texto = "   cortando os espaços    "
+
+print(texto + ".")
+print(texto.strip() + ".")
+#Ambos os espaços
+print(texto.lstrip() + ".")
+#left(esquerda)
+print(texto.rstrip() + ".")
+#right(direita)
+
+menu = "python"
+print(menu.center(14))
+#centraliza e os epeços acrescentados fica em branco
+print(menu.center(14, "#"))
+#cetralizada no meio de caracteres novos
+print("_".join(menu))
+#O caracter _ nao aparece depois do n porque n é o ultimo caracter.
